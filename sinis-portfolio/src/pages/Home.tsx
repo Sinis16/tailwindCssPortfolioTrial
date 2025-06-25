b26e78bd6fcba8c7aa6d387173f0a4e700615703
Santiago Sinisterra <s.sinisterra@uniandes.edu.co>
2025-06-25 15:29:23 -0500

    background

--- sinis-portfolio/src/pages/Home.tsx
+++ sinis-portfolio/src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* Navbar */}
+      {/* Hero Section */}
+      {/* Footer */}
+    </div>
+  );
 };
 
 export default Home;
